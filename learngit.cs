8c3e1a543f2bc9f81f0659987ecacf90e904d36e
ifeoluwa <ojikutuifeoluwa889@gmail.com>
2018-11-17 16:39:12 +0100

    [feazy] refactored code

--- learngit.cs
+++ learngit.cs
@@ -1,12 +1,11 @@
 public class learngit
 {
-
     public static void Main()
     {
-        var a = 34;
-        var b = 45;
-        var c = a+b;
-        Console:Writeline(c);
+        var  number one = 34;
+        var number two = 45;
+        var addition = one + two;
+        Console:Writeline( addition);
     }
 
 }
